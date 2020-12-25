96a7881b40944b3ff4ea399088b9cd785aa82457
pandeysaurabhofficial <60179545+pandeysaurabhofficial@users.noreply.github.com>
2020-12-25 17:30:58 +0530

    Day 1

---
+++ Day1(DSA Assmt).py
@@ -0,0 +1,18 @@
+1.
+{
+for(i=1;i<=n;i++)
+for(j=1;j<=n;j++)
+{
+ printf("hello")
+
+ans: it's time complexity is O(n square) as it's quadratic time complexity
+
+2.
+{
+for(i=1;i<=n;i*3)
+for(j=1;j<=n;j++)
+{
+ printf("hello")
+
+ans: it's time complexity is O(n square) as the value of n is unknown and its quadratic as well
+
